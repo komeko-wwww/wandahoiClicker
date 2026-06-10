860edb3f35b88887b05b50c694196544af5882db
kome <komeko.wwww@gmail.com>
2026-06-10 18:24:59 +0600

    imagelabel: Add Shaking effect on click
    what am i even doing

--- imagelabel.cpp
+++ imagelabel.cpp
@@ -1,5 +1,33 @@
 #include "imagelabel.hpp"
-ImageLabel::ImageLabel(QWidget *parent) : QLabel(parent) {}
+#define MLT 10 // mulitiplier value
+// Animation Parameters
+#define C_START 350 // Starting Duration
+#define C_END 200 // Ending Duration
+ImageLabel::ImageLabel(QWidget *parent) : QLabel(parent) {
+	// Initialize
+	shrink = new QPropertyAnimation(this, "geometry", this);
+	shrink->setDuration(C_START);
+
+	grow = new QPropertyAnimation(this, "geometry", this);
+	grow->setDuration(C_END);
+
+	QObject::connect(shrink, &QPropertyAnimation::finished, [this]() {
+		// Grow configuration
+		grow->setStartValue(geometry());
+		grow->setEndValue(this->original);
+		grow->start();
+	});
+}
+
 void ImageLabel::mousePressEvent(QMouseEvent *event) {
+	// Check if its first click
+	if (firstClick) {
+		original = geometry(); firstClick = false;
+	}
+
+	// Shrink Configuration
+	shrink->setStartValue(original);
+	shrink->setEndValue(original.adjusted(10 * MLT ,10 * MLT,-10 * MLT, -10 * MLT));
+	shrink->start();
 	emit clicked();
 }
--- imagelabel.hpp
+++ imagelabel.hpp
@@ -2,8 +2,13 @@
 #define IMAGE_LABEL_H
 #include<QLabel>
 #include<QMouseEvent>
+#include<QPropertyAnimation>
 class ImageLabel : public QLabel {
 	Q_OBJECT
+	private:
+		QPropertyAnimation *grow, *shrink;
+		QRect original;
+		bool firstClick = true;
 	public:
 		explicit ImageLabel(QWidget *parent = nullptr);
 	signals:
